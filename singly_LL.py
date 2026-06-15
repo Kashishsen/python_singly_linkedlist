c564e6ad2659eff80f4fd6fe962fad9eef132358
Kashishsen <kashishsen779@gmail.com>
2026-06-15 17:09:51 +0530

    added insertion and deletion operation in linked list

--- singly_LL.py
+++ singly_LL.py
@@ -25,10 +25,52 @@ class Single_linkedlist:
                 print(curr.val, end=" ")    
                 curr=curr.next
             print()
+    def insertion(self,val,position):
+        new_node=Node(val)
+        if position==0:
+            new_node.next=self.head
+            self.head=new_node
+        else:
+            current=self.head
+            prev_node=None
+            count=0
+            while current is not None and count<position:
+                prev_node=current
+                current=current.next
+                count+=1
+            prev_node.next=new_node
+            new_node.next=current        
+    def deletion(self,val):
+        temp=self.head
+        if temp.next is not None:
+            if temp.val==val:
+                self.head=temp.next
+                return
+            else:
+                found=False
+                prev=None
+                while temp is not None:
+                    if temp.val==val:
+                        found=True
+                        break
+                    prev=temp
+                    temp=temp.next
+                if found:
+                    prev.next=temp.next
+                    return
+                else:
+                    print("Node not found")
+
 sll=Single_linkedlist()
 sll.append(10)
 sll.append(20)
 sll.append(30)
-sll.traverse()                        
+sll.traverse()
+sll.insertion(40,2)
+sll.traverse()
+sll.deletion(40) 
+sll.traverse() 
+sll.deletion(60)
+                     
 
 
